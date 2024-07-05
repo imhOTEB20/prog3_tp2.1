a79a240acb88447ed4b33e083307668f2ec10423
Cristian Geronimo <crisalbertoaa20@gmail.com>
2024-07-05 03:01:11 -0300

    Se subio el ejercicio 3

--- ejercicio3/converter.js
+++ ejercicio3/converter.js
@@ -6,24 +6,78 @@ class Currency {
 }
 
 class CurrencyConverter {
-    constructor() {}
+    constructor(apiUrl) {
+        this.apiUrl = apiUrl;
+        this.currencies = [];
+    }
 
-    getCurrencies(apiUrl) {}
+    async getCurrencies() {
+        this.currencies = [];
+        try {
+            const response = await fetch(this.apiUrl + "/currencies");
+            if(!response.ok) {
+                throw new Error("ERROR al intentar realizar la solicitud. " + response.status)
+            }
+            const data = await response.json();
 
-    convertCurrency(amount, fromCurrency, toCurrency) {}
+            Object.entries(data)
+                .forEach(([code, name]) => {
+                    this.currencies.push(new Currency(code, name));
+            });
+        } catch (error) {
+            console.error('Se produjo un error al intentar realizar la solicitud. ', error)
+        }
+    }
+
+    async convertCurrency(amount, fromCurrency, toCurrency) {
+        if (fromCurrency.code === toCurrency.code)
+            return Number(amount);
+        else {
+            try {
+                const response = await fetch(`${this.apiUrl}/latest?amount=${amount}&from=${fromCurrency.code}&to=${toCurrency.code}`);
+                if (!response.ok) {
+                    throw new Error("ERROR al intentar realizar la solicitud. " + response.status);
+                }
+                const data = await response.json();
+                
+                return data["rates"][toCurrency.code];
+            } catch (error) {
+                console.error('Se produjo un error al intentar realizar la solicitud. ', error)
+            }
+        }
+    }
+
+    async convertCurrencyFromDate(amount, fromCurrency, toCurrency, date) {
+        if (fromCurrency.code === toCurrency.code)
+            return Number(amount);
+        else {
+            try {
+                const response = await fetch(`${this.apiUrl}/${date}?amount=${amount}&from=${fromCurrency.code}&to=${toCurrency.code}`);
+                if (!response.ok) {
+                    throw new Error("ERROR al intentar realizar la solicitud. " + response.status);
+                }
+                const data = await response.json();
+                return data["rates"][toCurrency.code];
+            } catch (error) {
+                console.error('Se produjo un error al intentar realizar la solicitud. ', error)
+            }
+        }
+    }
 }
-
+const converter = new CurrencyConverter("https://api.frankfurter.app");
 document.addEventListener("DOMContentLoaded", async () => {
     const form = document.getElementById("conversion-form");
     const resultDiv = document.getElementById("result");
     const fromCurrencySelect = document.getElementById("from-currency");
     const toCurrencySelect = document.getElementById("to-currency");
-
-    const converter = new CurrencyConverter("https://api.frankfurter.app");
+    const fromCurrencySelectDate = document.getElementById("from-currency-date");
+    const toCurrencySelectDate = document.getElementById("to-currency-date");
 
     await converter.getCurrencies();
     populateCurrencies(fromCurrencySelect, converter.currencies);
     populateCurrencies(toCurrencySelect, converter.currencies);
+    populateCurrencies(fromCurrencySelectDate, converter.currencies);
+    populateCurrencies(toCurrencySelectDate, converter.currencies);
 
     form.addEventListener("submit", async (event) => {
         event.preventDefault();
@@ -35,13 +89,11 @@ document.addEventListener("DOMContentLoaded", async () => {
         const toCurrency = converter.currencies.find(
             (currency) => currency.code === toCurrencySelect.value
         );
-
         const convertedAmount = await converter.convertCurrency(
             amount,
             fromCurrency,
             toCurrency
         );
-
         if (convertedAmount !== null && !isNaN(convertedAmount)) {
             resultDiv.textContent = `${amount} ${
                 fromCurrency.code
@@ -60,5 +112,47 @@ document.addEventListener("DOMContentLoaded", async () => {
                 selectElement.appendChild(option);
             });
         }
+    }
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const form = document.getElementById("conversion-form-date");
+    const resultDiv = document.getElementById("result-date");
+    const fromCurrencySelect = document.getElementById("from-currency-date");
+    const toCurrencySelect = document.getElementById("to-currency-date");
+    const dateInput = document.getElementById("date-input");
+    dateInput.max = formatDate(new Date());
+
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const amount = document.getElementById("amount-date").value;
+        const fromCurrency = converter.currencies.find(
+            (currency) => currency.code === fromCurrencySelect.value
+        );
+        const toCurrency = converter.currencies.find(
+            (currency) => currency.code === toCurrencySelect.value
+        );
+        const convertedAmount = await converter.convertCurrencyFromDate(
+            amount,
+            fromCurrency,
+            toCurrency,
+            formatDate(new Date(dateInput.value))
+        );
+        if (convertedAmount !== null && !isNaN(convertedAmount)) {
+            resultDiv.textContent = `${amount} ${
+                fromCurrency.code
+            } son ${convertedAmount.toFixed(2)} ${
+                toCurrency.code} en el ${dateInput.value}`;
+        } else {
+            resultDiv.textContent = "Error al realizar la conversión.";
+        }
+    });
+
+    function formatDate(date) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2,'0');
+        return `${year}-${month}-${day}`;
     }
 });
